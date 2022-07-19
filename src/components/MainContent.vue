f4748fc937655924ab2295735e29a55d506e4d85
LucaSpennato <luca.spennato@gmail.com>
2022-07-19 18:52:28 +0200

    testing page loader, only text

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,22 +1,33 @@
 <template>
   <main>
-    <div class="container p-5">
-        <div class="row col-10 m-auto text-center text-white " 
-        v-if="songs.length === 10" >
+    <div class="container p-5 text-center">
+        <div class="row">
+            <div class="row col-10 m-auto text-white" 
+            v-if="songs.length === 10" >
 
-            <SongCard
-            v-for="(song, index) in songs" :key="index"
-            :title="song.title"
-            :author="song.author"
-            :year="song.year"
-            :genre="song.genre"
-            :posterImg="song.poster"
-            />
+                <SongCard
+                v-for="(song, index) in songs" :key="index"
+                :title="song.title"
+                :author="song.author"
+                :year="song.year"
+                :genre="song.genre"
+                :posterImg="song.poster"
+                />
+            </div>
+
+            <div v-else class="col-2 m-auto fs-1 text-light">
+                <div class="page-loader">
+                    Ciao mi sto caricando
+                </div>
+            </div>
+
         </div>
 
-        <div v-else class="fs-1 text-light text-center">
-            ciao
-        </div>
+        <!-- <div class=" fs-1 text-dark py-5 my-5">
+            <div class="page-loader">
+                ciao
+            </div>
+        </div> -->
     </div>
   </main>
 </template>
@@ -63,6 +74,20 @@ export default {
     main{
         background-color: $mainBgColor;
         height: calc(100vh + 5rem);
+
+        .page-loader{
+            animation: 2s ease-out 0s infinite rotate;
+            width: 10rem;
+        }
+
+        @keyframes rotate{
+
+            100% {
+                transform: rotate(360deg);
+            }
+
+        }
+        
     }
 
 </style>
